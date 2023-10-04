312d3750d136d13465a1020dafdff67373b9437c
crow-editing <f.hayato0104@gmail.com>
2023-10-05 00:06:54 +0900

    API

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,5 +1,24 @@
+import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 
+const prisma = new PrismaClient();
+
+export async function main() {
+    try {
+        await prisma.$connect();
+    } catch (error) {
+        return Error("Failed to connect tp database");
+    }
+}
+
 export const GET = async (req: Request, res: NextResponse) => {
-    console.log("GET");
+    try {
+        await main();
+        const test = await prisma.test.findMany();
+        return NextResponse.json({message : "succeed", test }, {status : 200});
+    } catch (error) {
+        return NextResponse.json({message : "error", error }, {status : 500});
+    } finally {
+        await prisma.$disconnect();
+    }
 };
